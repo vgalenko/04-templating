1e8b8871c36a1fbd838911a312d2447417fc5e0a
Duane Rush <desertduane@yahoo.com>
2017-08-17 13:54:31 -0700

    template rendered, filters working, published day showing

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -2,7 +2,7 @@
 
 var articles = [];
 
-function Article (rawDataObj) {
+function Article(rawDataObj) {
   this.author = rawDataObj.author;
   this.authorUrl = rawDataObj.authorUrl;
   this.title = rawDataObj.title;
@@ -22,7 +22,7 @@ Article.prototype.toHtml = function() {
   //   Since your template can't hold any JS logic, we need to execute the logic here.
   //   The result is added to the object as a new property, which can then be referenced by key in the template.
   //   For example, you might want to display how old a post is, or say "(draft)" if it has no publication date:
-  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000);
   this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
 
   // DONE: Use the function that Handlebars gave you to return your filled-in html template for THIS article.
@@ -30,7 +30,7 @@ Article.prototype.toHtml = function() {
 
 };
 
-rawData.sort(function(a,b) {
+rawData.sort(function(a, b) {
   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
 });
 
@@ -38,6 +38,6 @@ rawData.forEach(function(articleObject) {
   articles.push(new Article(articleObject));
 });
 
-articles.forEach(function(article){
+articles.forEach(function(article) {
   $('#articles').append(article.toHtml());
 });
