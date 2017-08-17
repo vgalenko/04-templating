ed39f2cd6daa197d55ee7e956df6e8e093e3dc1a
Duane Rush <desertduane@yahoo.com>
2017-08-17 13:52:16 -0700

    almost done

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -12,7 +12,7 @@ function Article (rawDataObj) {
 }
 
 Article.prototype.toHtml = function() {
-  // TODO: Use handlebars to render your articles.
+  // DONE: Use handlebars to render your articles.
   //       - Get your template from the DOM.
   //       - Now "compile" your template with Handlebars.
   var template = $('#articles-template').html();
@@ -25,7 +25,7 @@ Article.prototype.toHtml = function() {
   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
   this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
 
-  // TODO: Use the function that Handlebars gave you to return your filled-in html template for THIS article.
+  // DONE: Use the function that Handlebars gave you to return your filled-in html template for THIS article.
   return templateRender(this);
 
 };
